5bf6717566e585fb4ebc1f51e604514eb9402a5d
crmgunter <crmgunter@gmail.com>
2018-08-28 11:46:24 -0400

    figured I might as well put this on github, just a little practice

---
+++ main.js
@@ -0,0 +1,61 @@
+const memory = [];
+let playerChoice = [];
+let checker = false;
+
+const computerTurn = () => {
+  const random = Math.floor(Math.random() * 4 + 1);
+  memory.push(random);
+  console.log(memory);
+  shine()
+};
+
+$('button').click(() => {
+    computerTurn()
+    $('button').addClass('none')
+})
+
+$(".player-button").click(function() {
+    console.log($(this).attr("id"))
+  playerChoice.push(parseInt($(this).attr("id")));
+  $(this).css('opacity', '.3')
+  setTimeout(() => {
+      $(this).css('opacity', '1')
+  }, 300)
+  if (playerChoice.length === memory.length) {
+    setTimeout(() => {
+        compareTwoArrays();
+    }, 800)    
+  }
+});
+
+const shine = () => {
+    memory.forEach((square, index) => {
+        setTimeout(() => {
+            console.log(square)
+            styleChosenSquares(square)
+        }, 1000 * index)
+    })
+}
+
+const styleChosenSquares = (choice) => {
+    const chosenSquare = $(`#${choice}`)
+    chosenSquare.css('opacity', '.3')
+    setTimeout(() => {
+        chosenSquare.css('opacity', '1')
+    }, 600)
+}
+
+const compareTwoArrays = () => {
+  playerChoice.forEach((arr1, i) => {
+    if (arr1 === memory[i]) {
+      checker = true;
+    } else {
+      $("body").html(`<div>YOU LOSE</div>
+        <button onClick="location.reload()">Play again?</button>`);
+    }
+  });
+  if ((checker = true)) {
+    playerChoice = [];
+    computerTurn();
+  }
+};
